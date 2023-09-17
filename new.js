6ae2aac6c7d4a8bf100fa0e8db9bcfb671c874c7
Shewanagithub <142712984+Shewanagithub@users.noreply.github.com>
2023-09-16 23:54:46 -0700

    Add files via upload

---
+++ new.js
@@ -0,0 +1,7 @@
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-bars');
+    navbvar.classList.toggle('open');
+}
